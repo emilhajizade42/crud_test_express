2bcaa6d622e489532ec974104a11117cc3c027e2
emilhajizade42 <emil.ph@code.edu.az>
2022-10-10 21:13:09 +0400

    add

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose');
 
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb+srv://emil:1e2m3i4l@cluster0.der8evf.mongodb.net/?retryWrites=true&w=majority');
+  
+  // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
+}
+const kittySchema = new mongoose.Schema({
+  name: String
+});
+const Kitten = mongoose.model('Kitten', kittySchema);
+const silence = new Kitten({ name: 'Silence' });
+console.log(silence.name); // 'Silence'
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
+  new Kitten({ name: 'ABC' }).save()
     res.send('Hello World!')
+
   })
  
   
